5256df84c6d2529f2574e683eb6ee246382c9aae
unknown <klimentmiroslav@gmail.com>
2023-12-12 09:34:10 +0100

    auth added

--- api_tests/deleteReservation.ts
+++ api_tests/deleteReservation.ts
@@ -1,8 +1,39 @@
 import { expect } from 'chai';
 
 describe('Delete Reservation API Test', () => {
+  let authToken: string;
+
+  before(() => {
+    // vytvoreni tokenu
+    const credentials = {
+      username: 'admin',
+      password: 'password123',
+    };
+
+    cy.request({
+      method: 'POST',
+      url: 'https://restful-booker.herokuapp.com/auth',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: credentials,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('token').that.is.a('string');
+      authToken = response.body.token;
+    });
+  });
+
   it('should delete an existing reservation', () => {
-    cy.request('DELETE', 'https://restful-booker.herokuapp.com/booking/1').then((response) => {
+    const reservationId = 1;
+
+    cy.request({
+      method: 'DELETE',
+      url: `https://restful-booker.herokuapp.com/booking/${reservationId}`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    }).then((response) => {
       expect(response.status).to.eq(201);
     });
   });
--- api_tests/updateReservation.ts
+++ api_tests/updateReservation.ts
@@ -1,7 +1,32 @@
 import { expect } from 'chai';
 
 describe('Update Reservation API Test', () => {
+  let authToken: string;
+
+  before(() => {
+    // vytvoreni tokenu
+    const credentials = {
+      username: 'admin',
+      password: 'password123',
+    };
+
+    cy.request({
+      method: 'POST',
+      url: 'https://restful-booker.herokuapp.com/auth',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: credentials,
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('token').that.is.a('string');
+      authToken = response.body.token;
+    });
+  });
+
   it('should update an existing reservation', () => {
+    const reservationId = 1;
+
     const updatedReservationData = {
       firstname: 'UpdatedFirstName',
       lastname: 'UpdatedLastName',
@@ -16,8 +41,11 @@ describe('Update Reservation API Test', () => {
 
     cy.request({
       method: 'PUT',
-      url: 'https://restful-booker.herokuapp.com/booking/1',
+      url: `https://restful-booker.herokuapp.com/booking/${reservationId}`,
       body: updatedReservationData,
+      headers: {
+        Authorization: `Bearer ${authToken}`, // Pridani tokenu do hlavicky pro autentizaci
+      },
     }).then((response) => {
       expect(response.status).to.eq(200);
     });
